1b4fcac94f7e7aca0fd48501514b9c699466ac77
Pawel Jaworski <javorski.pavel@gmail.com>
2021-03-26 00:53:12 +0100

    Login form without validation

--- src/pages/Auth/Login/Login.js
+++ src/pages/Auth/Login/Login.js
@@ -1,17 +1,41 @@
+import { useState } from 'react';
+import useAuth from '../../../hooks/useAuth';
+import { useHistory } from 'react-router-dom';
+import LoadingButton from '../../../components/UI/LoadingButton/LoadingButton';
+
 export default function Login(props) {
+  const [auth, setAuth] = useAuth();
+  const history = useHistory('');
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const submit = (e) => {
+    e.preventDefault();
+    setLoading(true)
+
+    setTimeout(() => {
+      //logowaanie
+
+      setAuth(true);
+      history.push('/')
+    }, 500);
+
+  }
     return (
       <div>
         <h2>Logowanie</h2>
-        <form>
+        <form onSubmit={submit}>
         <div className="form-group">
           <label>Email</label>
-          <input type="email" className="form-control" />
+          <input type="email" value={email} onChange={e => setEmail(e.target.value)} className="form-control" />
         </div>
         <div className="form-group">
           <label>Hasło</label>
-          <input type="password" className="form-control" />
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)} className="form-control" />
         </div>
-        <button className="btn btn-primary">Zaloguj</button>
+        <LoadingButton Loading={loading} label="Zaloguj"/>
       </form>
       </div>
     );
