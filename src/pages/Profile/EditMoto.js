10045cc61497df6c93c94940d48ff97b78d03074
Pawel Jaworski <javorski.pavel@gmail.com>
2021-04-15 18:56:42 +0200

    MotoForm

--- src/pages/Profile/EditMoto.js
+++ src/pages/Profile/EditMoto.js
@@ -1,9 +1,9 @@
-import axios from '../../../../axios';
+import axios from '../../axios';
 import { useHistory } from 'react-router-dom';
-import MotorcycleForm from '../MotorcycleForm';
+import MotorcycleForm from '../Profile/MotorcycleForm'
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import useAuth from '../../../../hooks/useAuth';
+import useAuth from '../../hooks/useAuth';
 
 const EditMoto = props => {
   const [auth] = useAuth();
@@ -20,7 +20,7 @@ const EditMoto = props => {
     const res = await axios.get(`/motorcycles/${id}.json`);
     const motorcycleData = res.data;
 
-    delete(motorcycleData.user_id);
+    delete(motorcycleData.userId);
     delete(motorcycleData.rating);
 
     setMotorcycle(motorcycleData); 
